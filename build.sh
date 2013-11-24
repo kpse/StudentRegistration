36d98ffe29cd93d6df1629b8527bcf4c9b6a226f
Suo Qin <kpsesuo@gmail.com>
2013-11-25 02:47:40 +0800

    add paramters to build.sh, local building, push, deply

--- build.sh
+++ build.sh
@@ -2,5 +2,32 @@
 
 #npm install karma
 
-karma start --single-run && \
-play test
+
+#!/bin/sh
+
+function build_local {
+    karma start --single-run && \
+    play test
+}
+
+function build_and_push {
+    git pull --rebase && \
+    build_local && \
+    git push origin master
+}
+
+function deploy {
+    build_local && \
+    git push heroku master
+}
+
+function main {
+  	case $1 in
+		d) deploy ;;
+		p) build_and_push ;;
+		b) build_local ;;
+		*) build_local ;;
+	esac
+}
+
+main $@
